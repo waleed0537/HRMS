877b1b8d1e04bfdd2c8ad8d603df9f730da9080e
waleed0537 <133221815+waleed0537@users.noreply.github.com>
2025-02-08 18:42:10 +0500

    Added onrender in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensures Vite binds to all network interfaces
+    port: process.env.PORT || 5173, // Uses Render's assigned port or defaults to 5173
+    strictPort: true,
+    allowedHosts: ['.onrender.com'] // Allows all Render subdomains
+  }
 })

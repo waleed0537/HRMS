79d0113ca8c31d63a092e6e16e3f35ef54824ce3
waleed0537 <133221815+waleed0537@users.noreply.github.com>
2025-02-21 00:06:40 +0500

    Added useNavigate Import

--- src/Components/HRMSLadingPage.jsx
+++ src/Components/HRMSLadingPage.jsx
@@ -1,6 +1,6 @@
   import React, { useEffect, useState } from 'react';
   import { Link } from 'react-router-dom';
-
+  import { useNavigate } from 'react-router-dom';
   import 'bootstrap/dist/css/bootstrap.min.css';
   import '@fortawesome/fontawesome-free/css/all.min.css';
   import WOW from 'wow.js';

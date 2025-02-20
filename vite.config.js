fed37fdd1668372b61479beb392d66aca451974b
waleed0537 <133221815+waleed0537@users.noreply.github.com>
2025-02-20 23:56:45 +0500

    Adjusted Redering

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['jquery']
+    }
+  },
   server: {
     host: '0.0.0.0', // Ensures Vite binds to all network interfaces
     port: process.env.PORT || 5173, // Uses Render's assigned port or defaults to 5173

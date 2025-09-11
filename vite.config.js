1e7d05a2a2c9cf0acfb599c857e981a64823a248
waleed0537 <133221815+waleed0537@users.noreply.github.com>
2025-09-11 22:34:51 +0500

    Added backend

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+  import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['jquery']
+  // https://vite.dev/config/
+  export default defineConfig({
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        external: ['jquery']
+      }
+    },
+    server: {
+      host: '0.0.0.0', // Ensures Vite binds to all network interfaces
+      port: process.env.PORT || 5173, // Uses Render's assigned port or defaults to 5173
+      strictPort: true,
+      allowedHosts: ['hrrive.com', 'www.hrrive.com', 'hrms-sxi4.onrender.com','hrms-seab.onrender.com','hrms-backend-flfy.onrender.com','hrms-uqy9.onrender.com','hrms-backend-qoir.onrender.com']
     }
-  },
-  server: {
-    host: '0.0.0.0', // Ensures Vite binds to all network interfaces
-    port: process.env.PORT || 5173, // Uses Render's assigned port or defaults to 5173
-    strictPort: true,
-    allowedHosts: ['hrrive.com', 'www.hrrive.com', 'hrms-sxi4.onrender.com','hrms-seab.onrender.com','hrms-backend-flfy.onrender.com','hrms-uqy9.onrender.com']
-  }
-})
+  })

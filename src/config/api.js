339a1c88ef06cd772650d546c0498223065112ea
waleed0537 <133221815+waleed0537@users.noreply.github.com>
2025-09-11 22:58:01 +0500

    Changed again

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@
 
 // export default API_BASE_URL;
 const API_BASE_URL = import.meta.env.PROD 
-  ? 'https://hrms-backend-qoir.onrender.com/api'
+  ? 'https://hrms-backend-qoir.onrender.com'
   : 'http://localhost:5000';
 
 export default API_BASE_URL;
